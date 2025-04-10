a4ddb34c4ef4b13fbbb681d67e39e59dfecef48f
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-11 00:04:03 +0100

    Refactor: rename InvoiceEntry component to InvoiceMeta

---
+++ src/components/desktop/InvoiceMeta.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+import { useAppContext } from "../../context/AppContext";
+import InputField from "../common/InputField";
+
+const InvoiceMeta = () => {
+
+    const { state: { dFault, invoiceNum, }, handleCustomInputs } = useAppContext();
+
+    return (
+        <Col lg={4} md={4} className="invoiceEntry">
+            <div>
+                <InputField
+                    value={dFault}
+                    onChange={(value) => handleCustomInputs('ALTER_DEFAULT', value)}
+                />
+
+            </div>
+            <div className='invoice-no-div'>
+                <label htmlFor='1'>#</label>
+                <InputField
+                    id='1'
+                    value={invoiceNum}
+                    onChange={(value) => handleCustomInputs('INVOICE_NUMBER', value)}
+                    className='text-end'
+                    placeholder="1"
+                />
+            </div>
+        </Col>
+    )
+}
+
+export default InvoiceMeta
