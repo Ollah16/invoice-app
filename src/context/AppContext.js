c8733b54bbcb4037729afc6ec1f888e5ee93a22c
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-10 15:52:46 +0100

    Refactor: add handleAddRow to value of AppProvider

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -140,7 +140,7 @@ export const AppProvider = ({ children }) => {
         }, 5000)
     }
 
-    return (<AppContext.Provider value={{ state, handleInputChange, handleDeleteRow }}>
+    return (<AppContext.Provider value={{ state, handleInputChange, handleDeleteRow, handleAddRow }}>
         {children}
     </AppContext.Provider>)
 }
