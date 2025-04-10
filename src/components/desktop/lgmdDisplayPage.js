537ddc890d0a10c3b7502ccb6aa73312e366ced8
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-10 16:06:03 +0100

    Refactor: create new mobile folder for desktop related files

---
+++ src/components/desktop/lgmdDisplayPage.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import CustomDataPage from "../../customInputPage";
+import TermAndCondPage from "../../term&Condtions";
+import InvoiceData from "./lgmdInvoiceDataPage";
+import DownloadBtnPage from "../../downloadSection";
+import DateTermsPage from "../../datePage";
+import InvInfo from "../../invoiceInformation";
+import InvoiceEntry from "../../invoiceEntry";
+import ImageComp from "../../imageComp";
+
+const LgMdDisplay = (
+    {
+        state,
+        handleInputValue,
+        handleDataInp,
+        handleInputButtons,
+        handleAddRow,
+        handleDeleteRow,
+        handleDownloadFunc,
+        handleCustomInputs,
+        handleInputsBtn,
+        handleLogo,
+        handleNavigation,
+        proceedDownload,
+        isDownload,
+        removeLogo }) => {
+
+
+    const {
+        billToTitle,
+        poNumber,
+        whoIsFrom,
+        address,
+        addressTitle,
+        dueDateTitle,
+        poTitle,
+        paymentTitle,
+        dateTitle,
+        billTo,
+        invoiceNum,
+        date,
+        dueDate,
+        paymentTerms,
+        logo } = state
+
+    return (<Row className='justify-content-between largeScreen'>
+
+        <Col lg={10} md={12} className="inner-content-col">
+            <Col lg={12} md={12} className='d-flex justify-content-between align-items-center'>
+
+                <ImageComp
+                    handleLogo={handleLogo}
+                    state={state}
+                    removeLogo={removeLogo} />
+
+                <InvoiceEntry
+                    state={state}
+                    handleCustomInputs={handleCustomInputs}
+                />
+
+            </Col>
+
+            <Col lg={12} md={12} className='d-flex justify-content-between gapInclude'>
+                <InvInfo
+                    state={state}
+                    handleCustomInputs={handleCustomInputs}
+                />
+
+                <DateTermsPage
+                    state={state}
+                    handleCustomInputs={handleCustomInputs}
+                />
+            </Col>
+
+            <InvoiceData />
+
+            <Row className="justify-content-between">
+
+                <TermAndCondPage
+                    state={state}
+                    handleCustomInputs={handleCustomInputs} />
+
+                <CustomDataPage
+                    handleInputsBtn={handleInputsBtn}
+                    handleCustomInputs={handleCustomInputs}
+                    handleDataInp={handleDataInp}
+                    handleInputValue={handleInputValue}
+                    handleInputButtons={handleInputButtons}
+                    state={state} />
+            </Row>
+        </Col>
+
+        <Col className="downloadSection">
+            <DownloadBtnPage
+                isDownload={isDownload}
+                handleDownloadFunc={handleDownloadFunc}
+                handleCustomInputs={handleCustomInputs}
+                handleNavigation={handleNavigation}
+                proceedDownload={proceedDownload}
+            />
+        </Col>
+    </Row >)
+}
+export default LgMdDisplay
---
+++ src/components/desktop/lgmdInvoiceDataPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+import LineButton from "../../lineItem";
+import DeleteRow from "../../deleteRow";
+import InputField from "../common/InputField";
+import { useAppContext } from "../../context/AppContext";
+
+const InvoiceData = () => {
+
+    const { state: { data }, handleInputChange, handleDeleteRow, handleAddRow } = useAppContext();
+
+    return (
+        <Col lg={12} md={12} className='invoice-data-table'>
+            <div>
+                <ul className="text-start">
+                    <li>#</li>
+                    <li>Description</li>
+                    <li>Quantity</li>
+                    <li>Rate</li>
+                    <li>Amount</li>
+                    <li className={data.length ? 'display' : 'd-none'}></li>
+                </ul>
+                {data.map(({ description, quantity, rate }, index) => (<ul key={index} className="table-inputs">
+                    <li>{index + 1}</li>
+                    <li><InputField value={description} onChange={(event) => handleInputChange('DESCRIPTION', event.target.value, index)} placeholder='item description' /></li>
+                    <li><InputField value={quantity} onChange={(event) => handleInputChange('QUANTITY', event.target.value, index)} placeholder='1' /></li>
+                    <li><InputField value={rate} onChange={(event) => handleInputChange('RATE', event.target.value, index)} placeholder='0' /></li>
+                    <li><InputField className="border-0" value={amount} disabled={true} /></li>
+                    {data.length && <li ><DeleteRow handleDeleteRow={handleDeleteRow} index={index} state={state} /></li>}
+                </ul>))}
+            </div>
+
+            <LineButton handleAddRow={handleAddRow} />
+        </Col >)
+}
+export default InvoiceData
