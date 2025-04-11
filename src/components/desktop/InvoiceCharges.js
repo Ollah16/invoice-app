98d8ff90cc6e7b11065cb360740ffaa6a6068c37
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-11 18:04:35 +0100

    Refactor: update responsiveness with class utility

--- src/components/desktop/InvoiceCharges.js
+++ src/components/desktop/InvoiceCharges.js
@@ -10,50 +10,49 @@ const InvoiceCharges = () => {
 
     const { state: { discountTitle, taxTitle, shippingTitle, isShipping, isDiscount, isTax, taxAmount, shippingAmount, discountAmount }, handleCustomInputs, handleInputValue, handleInputsBtn } = useAppContext()
 
-    return (<Row>
+    return (<Col lg={12} md={12} className='my-2'>
 
         {isDiscount === 1 &&
-            <Col lg={12} md={12} className='custom-col'>
-                <div className='customInput'>
+            <Row className='p-0 justify-content-between my-1'>
+                <Col lg={7} md={7} className='label-input'>
                     <InputField value={discountTitle} onChange={(value) => handleCustomInputs('DISCOUNT_TITLE', value)} className='text-end' placeholder='Discount' />
-                </div>
-                <div className='customInputbutton'>
-                    <InputField className='text-end'
+                </Col>
+                <Col lg={5} md={5} className='charges-input-value d-flex justify-content-end p-0'>
+                    <InputField className='text-end w-100'
                         value={discountAmount}
                         onChange={(value) => handleInputValue('DISCOUNT', value)} placeholder='%' />
                     <Button className='cancel-button'
                         onClick={() => handleInputsBtn('IS_NOT_DISCOUNT')}>
                         <span><RxCross2 /></span>
                     </Button>
-                </div>
-            </Col>}
+                </Col>
+            </Row>}
 
         {
             isTax === 1 &&
-            <Col lg={12} md={12} className='custom-col'>
-                <div className='customInput'>
-
+            <Row className='p-0 justify-content-between my-1'>
+                <Col lg={7} md={7} className='label-input'>
                     <InputField value={taxTitle} onChange={(value) => handleCustomInputs('TAX_TITLE', value)} className='text-end' placeholder='Tax' />
-                </div>
-                <div className='customInputbutton'>
-                    <InputField className='text-end'
+                </Col>
+                <Col lg={5} md={5} className='charges-input-value d-flex justify-content-end p-0'>
+                    <InputField className='text-end w-100'
                         value={taxAmount}
                         onChange={(value) => handleInputValue('TAX', value)} />
                     <Button className='cancel-button'
                         onClick={() => handleInputsBtn('IS_NOT_TAX')}>
                         <span><RxCross2 /></span>
                     </Button>
-                </div>
-            </Col>
+                </Col>
+            </Row>
         }
 
         {
             isShipping === 1 &&
-            <Col lg={12} md={12} className='custom-col'>
-                <div className='customInput'>
+            <Row className='p-0 justify-content-between my-1'>
+                <Col lg={7} md={7} className='label-input'>
                     <InputField value={shippingTitle} onChange={(value) => handleCustomInputs('SHIPPING_TITLE', value)} className='text-end' placeholder='Shipping' />
-                </div>
-                <div className='customInputbutton'>
+                </Col>
+                <Col lg={5} md={5} className='charges-input-value d-flex justify-content-end p-0'>
                     <InputField className='text-end'
                         value={shippingAmount}
                         onChange={(value) => handleInputValue('SHIPPING', value)} />
@@ -61,27 +60,27 @@ const InvoiceCharges = () => {
                         onClick={() => handleInputsBtn('IS_NOT_SHIPPING')}>
                         <span><RxCross2 /></span>
                     </Button>
-                </div>
-            </Col>
+                </Col>
+            </Row>
         }
 
 
-        <Col lg={12} md={12} className='custom-btn'>
-            {isDiscount === 0 &&
-                <Button onClick={() => handleInputsBtn('IS_DISCOUNT')} >
+        <Col lg={12} md={12} className={`d-flex justify-content-end charges-button`}>
+            {isDiscount === 0 && (
+                <Button onClick={() => handleInputsBtn('IS_DISCOUNT')} className="d-flex justify-content-between align-items-center border-0 bg-transparent fw-semibold p-0 ms-3">
                     <span><IoMdAdd size={15} /></span><span>Discount</span>
-                </Button>}
-            {isTax === 0 &&
-                <Button onClick={() => handleInputsBtn('IS_TAX')} >
+                </Button>)}
+            {isTax === 0 && (
+                <Button onClick={() => handleInputsBtn('IS_TAX')} className="d-flex justify-content-between align-items-center border-0 bg-transparent fw-semibold p-0 ms-3">
                     <span><IoMdAdd size={15} /></span><span>Tax</span>
-                </Button>}
+                </Button>)}
             {isShipping === 0 &&
-                <Button onClick={() => handleInputsBtn('IS_SHIPPING')} >
+                (<Button onClick={() => handleInputsBtn('IS_SHIPPING')} className="d-flex justify-content-between align-items-center border-0 bg-transparent fw-semibold p-0 ms-3">
                     <span><IoMdAdd size={15} /></span><span>Shipping</span>
-                </Button>}
+                </Button>)}
         </Col>
 
-    </Row>)
+    </Col>)
 }
 
 export default InvoiceCharges
