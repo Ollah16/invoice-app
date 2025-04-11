0bea4182e6d32c9eef3d0e0dc8a38d84336410b0
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-11 14:18:49 +0100

    Refactor: change class names and use Bootstrap element

--- src/components/desktop/InvoiceContactSection.js
+++ src/components/desktop/InvoiceContactSection.js
@@ -8,50 +8,50 @@ const InvoiceContactSection = () => {
     const { state: { whoIsFrom, billTo, billToTitle, address, addressTitle }, handleCustomInputs } = useAppContext();
 
     return (
-        <Col >
-            <Col lg={9} md={9} className="custom-input-col">
-                <div className="norm-Input-div">
+        <Col lg={7} className="d-flex flex-column gap-2">
+            <Row lg={9} md={9}>
+                <Col className="contact-value-input">
                     <InputField
                         onChange={(value) => handleCustomInputs('WHOISFROM', value)}
                         value={whoIsFrom}
                         type="text"
                         placeholder="Who is the invoice from? (required)" />
-                </div>
-            </Col>
+                </Col>
+            </Row>
 
-            <Row className='justify-content-around smadjust'>
-                <Col lg={6} md={6} className="custom-input-col">
-                    <div className="custom-input-div">
+            <Row className='justify-content-around'>
+                <Col lg={6} md={6}>
+                    <Col className="label-input mb-1">
                         <InputField
                             value={billToTitle}
                             onChange={(value) => handleCustomInputs('BILL_TITLE', value)}
                         />
-                    </div>
-                    <div className="norm-Input-div">
+                    </Col>
+                    <Col className="contact-value-input">
                         <InputField
                             onChange={(value) => handleCustomInputs('BILL', value)}
                             value={billTo}
                             type="text"
                             placeholder="Who is the invoice to?(required)"
                         />
-                    </div>
+                    </Col>
                 </Col>
 
-                <Col lg={6} md={6} className="custom-input-col">
-                    <div className="custom-input-div">
+                <Col lg={6} md={6}>
+                    <Col className="label-input mb-1">
                         <InputField value={addressTitle} onChange={(value) => handleCustomInputs('ADDRESS_TITLE', value)} />
-                    </div>
-                    <div className="norm-Input-div">
+                    </Col>
+                    <Col className="contact-value-input">
                         <InputField
                             onChange={(value) => handleCustomInputs('ADDRESS', value)}
                             type="text"
                             value={address}
                             placeholder="(optional)"
                         />
-                    </div>
+                    </Col>
                 </Col>
             </Row>
-        </Col>
+        </Col >
     )
 }
 
