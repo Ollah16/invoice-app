1e6d92b4dd7d9c7bcb0f6de1d1f8ec6750542563
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-11 18:03:14 +0100

    Refactor: update responsiveness

--- src/components/desktop/InvoiceSummary/InvoiceTotal.js
+++ src/components/desktop/InvoiceSummary/InvoiceTotal.js
@@ -9,19 +9,19 @@ const InvoiceTotal = () => {
 
     return (<Col lg={12} md={12} className='d-flex flex-column gap-2'>
         <Row className='align-items-center justify-content-between'>
-            <Col lg={7} md={6} className='label-input'>
+            <Col lg={7} md={7} className='label-input'>
                 <InputField value={totalTitle} onChange={(value) => handleCustomInputs('TOTAL_TITLE', value)} className='text-end' placeholder='Total' />
             </Col>
-            <Col lg={5} md={6} className='payment-input-value text-end justify-content-end overflow-auto'>
+            <Col lg={5} md={5} className='payment-input-value text-end justify-content-end overflow-auto'>
                 {`${currency.toString().substring(0, 3)}${total}.00`}
             </Col>
         </Row>
 
         <Row className='align-items-center justify-content-between'>
-            <Col lg={7} md={6} className='label-input'>
+            <Col lg={7} md={7} className='label-input'>
                 <InputField value={amountPaidTitle} onChange={(value) => handleCustomInputs('AMOUNT_PAID_TITLE', value)} className='text-end' placeholder='Amount Paid' />
             </Col>
-            <Col lg={5} md={6} className='payment-input-value'>
+            <Col lg={5} md={5} className='payment-input-value ps-0'>
                 <InputField
                     value={amountPaid}
                     onChange={(value) => handleInputValue('AMOUNT_PAID', value)}
@@ -30,10 +30,10 @@ const InvoiceTotal = () => {
         </Row>
 
         <Row className='align-items-center justify-content-between'>
-            <Col lg={7} md={6} className='label-input'>
+            <Col lg={7} md={7} className='label-input'>
                 <InputField value={balanceDueTitle} onChange={(value) => handleCustomInputs('BALANCE_DUE_TITLE', value)} className='text-end' placeholder='Balance Due' />
             </Col>
-            <Col lg={5} md={6} className='payment-input-value text-end d-flex justify-content-end overflow-auto'>
+            <Col lg={5} md={5} className='payment-input-value text-end d-flex justify-content-end overflow-auto'>
                 {`${currency.toString().substring(0, 3)}${balance}.00`}
             </Col>
         </Row >
