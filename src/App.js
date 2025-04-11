3c4adf2697c2efb7a90de41d4f3a70afd99f636f
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-11 11:16:04 +0100

    Refactor: update App.js with AppRoutes

--- src/App.js
+++ src/App.js
@@ -1,67 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import './modal.css'
 import './thanksPage.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DownloadPage from './downloadPage';
-import HomePage from './homePage';
-import RegisterPage from './authenticatePage';
-import RecordPage from './recordPage';
-import ThanksPage from './thanksPage';
 import { AppProvider } from './context/AppContext';
+import AppRoutes from './AppRoutes';
 
 const App = () => {
 
   return (
     <AppProvider>
-      <Routes>
-        <Route
-          path='/*' element={<HomePage
-            proceedDownload={proceedDownload}
-            handleLogo={handleLogo}
-            handleSignOut={handleSignOut}
-            handleInputValue={handleInputValue}
-            handleDataInp={handleDataInp}
-            state={state}
-            toggleAuth={toggleAuth}
-            handleCustomInputs={handleCustomInputs}
-            handleAddRow={handleAddRow}
-            handleDeleteRow={handleDeleteRow}
-            handleInputsBtn={handleInputsBtn}
-            handleNavigation={handleNavigation}
-            handleMessage={handleMessage}
-            removeLogo={removeLogo}
-          />} />
-
-        <Route path='/download'
-          element={<DownloadPage
-            state={state}
-            handleClearState={handleClearState}
-            handleNavigation={handleNavigation}
-          />} />
-
-        <Route path='/authenticate/:page'
-          element={<RegisterPage
-            state={state}
-            handleAuth={handleAuth}
-            handleNavigation={handleNavigation}
-            handleMessage={handleMessage}
-          />} />
-
-        <Route path='/records'
-          element={<RecordPage
-            state={state}
-            handleNavigation={handleNavigation} />} />
-
-        <Route path='/thanks'
-          element={<ThanksPage
-            toggleAuth={toggleAuth}
-            handleNavigation={handleNavigation}
-            state={state}
-          />
-          } />
-      </Routes>
+      <AppRoutes />
     </AppProvider>
   )
 }
