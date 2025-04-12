12affb038d7c6cca7229cb6f3d5d0ed2a8a4d509
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-12 14:43:20 +0100

    feat(inputField): add prop fieldName, add context, and error element block

--- src/components/common/InputField.js
+++ src/components/common/InputField.js
@@ -1,16 +1,30 @@
 import React from 'react'
+import { useAppContext } from '../../context/AppContext'
 
-const InputField = ({ type, onChange = () => { }, value, placeholder, ariaLabel, className, id }) => {
+const InputField = ({ type, onChange = () => { }, onBlur, value, placeholder, ariaLabel, className, id, fieldName = '' }) => {
+
+    const { state: { error } } = useAppContext();
+
+    const errorfield = error[fieldName.toLowerCase()];
+
     return (
-        <input
-            onChange={(e) => onChange(e.target.value)}
-            id={id}
-            type={type}
-            value={value}
-            placeholder={placeholder}
-            aria-label={ariaLabel}
-            className={`${className} overflow-auto p-1`}
-        />
+        <div>
+            <input
+                onChange={(e) => onChange(e.target.value)}
+                onBlur={onBlur}
+                id={id}
+                type={type}
+                value={value}
+                placeholder={placeholder}
+                aria-label={ariaLabel}
+                className={`${className} overflow-auto p-1`}
+            />
+            {errorfield && (
+                <span>
+                    {errorfield}
+                </span>
+            )}
+        </div>
     )
 }
 
