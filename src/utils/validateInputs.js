33af576dee6e818361a15a2d49581607de65d8f0
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-12 14:32:25 +0100

    feat(utils): add WHOISFROM case in the helper function

--- src/utils/validateInputs.js
+++ src/utils/validateInputs.js
@@ -1,4 +1,13 @@
 
-export const handleInputValidation = () => {
+export const handleInputValidation = (type, value) => {
 
+    switch (type) {
+        case "WHOISFROM":
+            if (!value.trim()) {
+                return "Please enter the name of who the invoice is from.";
+            }
+            break;
+        default:
+            return ""
+    }
 }
