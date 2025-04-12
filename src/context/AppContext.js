981bb4ffc98795c5179b00506d2473a1f0617e9f
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-12 15:41:28 +0100

    refactor(appContext): remove handleNavigation function

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useReducer, useState } from "react";
 import myReducer, { initialState } from "../useReducer/reducer";
-import { useNavigate } from "react-router-dom";
 import { handleInputValidation } from "../utils/validateInputs";
 
 const AppContext = createContext();
@@ -60,8 +59,6 @@ export const AppProvider = ({ children }) => {
         ERROR: 'ERROR'
     }
 
-    const navigate = useNavigate()
-
     const handleBlur = (fieldName, value) => {
         const error = handleInputValidation(fieldName, value);
         dispatch({ type: actionTypes.ERROR, payload: { fieldName, error } })
@@ -99,32 +96,8 @@ export const AppProvider = ({ children }) => {
         dispatch({ type: actionTypes.IS_LOGGED });
     };
 
-    const handleAuthentication = (type) => {
-        handleNavigation(`/authenticate/${type}`)
-    }
-
     const handleClearState = () => {
         dispatch({ type: actionTypes.CLEAR_STATE })
-    }
-
-    const handleNavigation = (page) => {
-
-        if (page.includes('records')) {
-
-            navigate('authenticate/homepage')
-
-        } else if (page.includes('clearState')) {
-
-            handleClearState()
-            navigate('/')
-
-        } else if (page.includes('thanks')) {
-            navigate(page)
-
-        } else {
-            navigate(page)
-        }
-
     }
 
     const handleSignOut = () => {
@@ -153,7 +126,7 @@ export const AppProvider = ({ children }) => {
         }, 5000)
     }
 
-    return (<AppContext.Provider value={{ state, handleDataChange, handleDeleteRow, handleAddRow, handleLogo, handleRemoveLogo, handleCustomInputs, handleInputValue, handleInputsBtn, handleNavigation, proceedDownload, isDownload, handleDownload, handleAuthentication, handleBlur }}>
+    return (<AppContext.Provider value={{ state, handleDataChange, handleDeleteRow, handleAddRow, handleLogo, handleRemoveLogo, handleCustomInputs, handleInputValue, handleInputsBtn, proceedDownload, isDownload, handleDownload, handleBlur }}>
         {children}
     </AppContext.Provider>)
 }
