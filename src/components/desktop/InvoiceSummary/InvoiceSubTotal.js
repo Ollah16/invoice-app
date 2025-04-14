1b5b1b1f26ddf742510ba2e34fb814e454c00ebe
Ollah16 <olaoluwaboluwatife@yahoo.com>
2025-04-14 22:05:41 +0100

    refactor(input): add attributes(type, aria-label) to input

--- src/components/desktop/InvoiceSummary/InvoiceSubTotal.js
+++ src/components/desktop/InvoiceSummary/InvoiceSubTotal.js
@@ -10,7 +10,13 @@ const InvoiceSubTotal = () => {
         <Col lg={12} md={12} >
             <Row className='justify-content-between align-items-center'>
                 <Col lg={7} md={7} className='label-input'>
-                    <InputField value={subTotalTitle} onChange={(value) => handleCustomInputs('SUB_TOTAL_TITLE', value)} className='text-end' placeholder='Subtotal' />
+                    <InputField
+                        value={subTotalTitle}
+                        onChange={(value) => handleCustomInputs('SUB_TOTAL_TITLE', value)}
+                        type='text'
+                        className='text-end'
+                        aria-label='Sub Total'
+                        placeholder='Subtotal' />
                 </Col>
                 <Col lg={5} md={5} className='d-flex justify-content-end overflow-auto'>
                     {`${currency.toString().substring(0, 3)}${subTotal}.00`}
